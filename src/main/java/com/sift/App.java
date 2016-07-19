d159f4513e8248fae393b90e339088322e722f6b
joyghosh <joy.ghosh@tiatros.com>
2016-07-19 12:58:32 +0530

    Classification and threshold

--- src/main/java/com/sift/App.java
+++ src/main/java/com/sift/App.java
@@ -18,13 +18,23 @@ public class App
         Util.sampleTrain(classifier);
 //        classifier.train("the quick brown fox jumps over the lazy dog", "good");
 //        classifier.train("make quick money in the online casino","bad");
-        System.out.println(classifier.featureCount("jumps", "bad"));
-        System.out.println(classifier.featureCount("jumps", "good"));
-        System.out.println(classifier.featureProbability("fox", "good"));
-        System.out.println(classifier.weightedProbability("money", "good",1.0, 0.5));
-        Util.sampleTrain(classifier);
-        System.out.println(classifier.weightedProbability("money", "good",1.0, 0.5));
-        Util.sampleTrain(classifier);
-        System.out.println(classifier.weightedProbability("money", "good",1.0, 0.5));
+//        System.out.println(classifier.featureCount("jumps", "bad"));
+//        System.out.println(classifier.featureCount("jumps", "good"));
+//        System.out.println(classifier.featureProbability("fox", "good"));
+//        System.out.println(classifier.weightedProbability("money", "good",1.0, 0.5));
+//        Util.sampleTrain(classifier);
+//        System.out.println(classifier.weightedProbability("money", "good",1.0, 0.5));
+//        Util.sampleTrain(classifier);
+//        System.out.println(classifier.weightedProbability("money", "good",1.0, 0.5));
+//        System.out.println(((NaiveBayes)classifier).probability("quick money", "good"));
+//        System.out.println(((NaiveBayes)classifier).probability("quick money", "bad"));
+         System.out.println(((NaiveBayes)classifier).classify("quick rabbit", "unknown"));
+         System.out.println(((NaiveBayes)classifier).classify("quick money", "unknown"));
+         ((NaiveBayes)classifier).setThreshold("bad", 3.0);
+         System.out.println(((NaiveBayes)classifier).classify("quick money", "unknown"));
+         for(int i=0;i<10;i++){
+        	 Util.sampleTrain(classifier);
+         }
+         System.out.println(((NaiveBayes)classifier).classify("quick money", "unknown"));
     }
 }
--- src/main/java/com/sift/classifiers/NaiveBayes.java
+++ src/main/java/com/sift/classifiers/NaiveBayes.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.sift.utilities.Util;
+
 /**
  * A naive document classifier based on Bayes theorem.
  * Extends the <code> {@link BaseClassifier} </code> model.
@@ -27,4 +29,84 @@ public class NaiveBayes extends BaseClassifier {
 		this.thresholds = new HashMap<String, Double>();
 	}
 	
+	/**
+	 * Document probability.
+	 * @param item
+	 * @param category
+	 * @return
+	 */
+	private double documentProbability(Object item, String category){
+		String[] features = (String[]) Util.getFeatures(item, type);
+		double p = 1.0;
+		for(String feature:features){
+			p *= weightedProbability(feature, category, 1.0, 0.5);
+		}
+		return p;
+	}
+	
+	/**
+	 * Probability of a category.
+	 * @param item
+	 * @param category
+	 * @return
+	 */
+	public double probability(Object item, String category){
+		double category_probability = categoryCount(category)/totalCount();
+		double doc_probability = documentProbability(item, category);
+		return (doc_probability*category_probability);
+	}
+	
+	/**
+	 * Set threshold value for category.
+	 * @param category
+	 * @param threshold
+	 */
+	public void setThreshold(String category, double threshold){
+		thresholds.put(category, threshold);
+	}
+	
+	/**
+	 * Return threshold value for a category.
+	 * @param category
+	 * @return
+	 */
+	public double getThreshold(String category){
+		
+		double threshold = 1.0;
+		if(thresholds.containsKey(category)){
+			threshold = thresholds.get(category);
+		}
+		return threshold;
+	}
+	
+	/**
+	 * Classify item.
+	 * @param item
+	 * @param _default
+	 * @return
+	 */
+	public String classify(Object item, String _default){
+		
+		Map<String, Double> probs = new HashMap<String, Double>();
+		
+		//Find the category with the highest probability.
+		double max = 0.0; String best = null;
+		for(String category:categories()){
+			probs.put(category, probability(item, category));
+			if(probs.get(category) > max){
+				max = probs.get(category);
+				best = category;
+			}
+		}
+		
+		//Make sure the probability exceeds threshold*next best.
+		for(String category:probs.keySet()){
+			if(category.equalsIgnoreCase(best)) continue;
+			if(probs.get(category)*getThreshold(category) > probs.get(best)){
+				return _default;
+			}
+		}
+		
+		return best;
+	}
 }
